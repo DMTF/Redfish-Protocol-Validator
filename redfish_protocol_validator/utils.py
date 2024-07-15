c9e85e8915206a7fd8f68100b089e170ddf08d16
Mike Raineri <michael.raineri@dell.com>
2024-07-15 16:30:47 -0400

    Added dummy body
    Signed-off-by: Mike Raineri <michael.raineri@dell.com>

--- redfish_protocol_validator/utils.py
+++ redfish_protocol_validator/utils.py
@@ -100,6 +100,7 @@ def build_exception_response(exception, uri, method):
     response.url = uri
     response.request = requests.Request()
     response.request.method = method
+    response.request.body = None
     logging.error('%s on %s caused %s exception; building HTTP 600 '
                   'response' % (method, uri, type(exception).__name__))
     return response
